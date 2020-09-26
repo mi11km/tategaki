65971e47aff9e9336e612ece337be24599be57f4
mi11km <mi11km.basket@gmail.com>
2020-09-26 18:15:29 +0900

    improved

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* 任意設定する変数 */
 const bookTypesNum = 100; /* todo 取ってくる本の幅: 1以上101未満の数字 */
-const appearanceRate = 700;
-const animationTime = 5000; // cssのanimationTimeと一致させる！（こっちはms）
+const appearanceRate = 1000;
+const animationTime = 8000; // cssのanimationTimeと一致させる！（こっちはms）
 
 getWeatherInfo().then((res) => {
   console.log(res);
@@ -21,14 +21,26 @@ async function getSentence() {
 }
 
 function createSentenceElement(sentence) {
+  let left = Math.random() * 98;
+  const leftDiv = (left - 2.0).toFixed(2) + "%";
+  left = left.toFixed(2) + "%";
+  const top = (Math.random() * 60).toFixed(2) + "%";
+
+  let div = document.createElement("div");
+  div.setAttribute("class", "water");
+  document.body.appendChild(div);
+  div.style.left = leftDiv;
+  div.style.top = top;
+
   let h2 = document.createElement("h2");
   h2.setAttribute("class", "sentence");
   document.body.appendChild(h2);
   h2.innerHTML = sentence;
-  h2.style.left = (Math.random() * 98).toFixed(2) + "%";
-  h2.style.top = (Math.random() * 60).toFixed(2) + "%";
+  h2.style.left = left;
+  h2.style.top = top;
 
   setTimeout(function () {
+    document.body.removeChild(div);
     document.body.removeChild(h2);
   }, animationTime);
 }
@@ -47,9 +59,29 @@ function createSentence() {
 
 /**/
 
+const getPresentLocation = new Promise((resolve) => {
+  let position = {};
+
+  function success(pos) {
+    position.lat = pos.coords.latitude;
+    position.lon = pos.coords.longitude;
+    resolve(position);
+  }
+
+  function fail(error) {
+    alert("位置情報の取得に失敗しました。エラーコード：" + error.code);
+  }
+
+  navigator.geolocation.getCurrentPosition(success, fail);
+});
+
 async function getWeatherInfo() {
   const apiKey = "e746bc30f40c60f304a46eb058138b94"; /* todo 隠蔽すべき */
   const baseURL = "https://api.openweathermap.org/data/2.5/onecall?";
+
+  // getPresentLocation.then((position) => {
+  //   console.log(position.lat);
+  // });
 
   const query = new URLSearchParams({
     // 東京駅　todo 現在位置にする
