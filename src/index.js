f6fa40105637b06ee42a6d5fc0415d0a7e433d51
mi11km <mi11km.basket@gmail.com>
2020-09-27 11:48:33 +0900

    improved

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 /* 任意設定する変数 */
-const bookTypesNum = 100; /* todo 取ってくる本の幅: 1以上101未満の数字 */
+const bookTypesNum = 500; /* todo 取ってくる本の幅: 1以上101未満の数字 */
 const animationTime = 8000; // cssのanimationTimeと一致させる！（こっちはms）
 const fetchWeatherInfoInterval = 3600000; // 1時間おき
 
 let interval;
+const maxRainfall = 88.7;
+const positionLeft = [];
 
 setSentenceByWeatherInfo();
 setInterval(setSentenceByWeatherInfo, fetchWeatherInfoInterval);
@@ -15,11 +17,14 @@ function setSentenceByWeatherInfo() {
     clearInterval(interval);
   }
   getWeatherInfo().then((res) => {
-    let appearanceRate = 2000;
+    let appearanceRate = 6000;
     if ("rain" in res.current) {
-      appearanceRate *= res.current.rain["1h"];
+      if (res.current.rain["1h"] > 20) {
+        appearanceRate *= 1 - res.current.rain["1h"] / maxRainfall;
+      }
       console.log(appearanceRate);
       interval = setInterval(createSentence, appearanceRate);
+      return;
     }
     interval = setInterval(createSentence, appearanceRate);
   });
@@ -28,42 +33,63 @@ function setSentenceByWeatherInfo() {
 /**/
 
 async function getSentence() {
-  const id = Math.round(Math.random() * bookTypesNum) + 3;
-  const res = await fetch(`https://api.bungomail.com/v0/books/${id}`);
-  const json = await res.json();
+  let id = Math.round(Math.random() * bookTypesNum) + 3;
+  let res = await fetch(`https://api.bungomail.com/v0/books/${id}`);
+  let json = await res.json();
 
-  if (!"書き出し" in json.book || !"姓名" in json.book) {
-    return;
+  while (!("書き出し" in json.book) && !("作品名" in json.book)) {
+    id = Math.round(Math.random() * bookTypesNum) + 3;
+    res = await fetch(`https://api.bungomail.com/v0/books/${id}`);
+    json = await res.json();
   }
   return json;
 }
 
+function isOverlapSentence(left) {
+  for (let i = 0; i < positionLeft.length; i++) {
+    if (positionLeft[i] - 5.0 < left && left < positionLeft[i] + 5.0) {
+      return true;
+    }
+  }
+  return false;
+}
+
 function createSentenceElement(sentence) {
-  const left = (Math.random() * 98).toFixed(2) + "%";
   const top = (Math.random() * 60).toFixed(2) + "%";
+  let left = Math.random() * 98;
+
+  if (positionLeft) {
+    while (isOverlapSentence(left)) {
+      left = Math.random() * 98;
+    }
+  }
+  positionLeft.push(left);
+  console.log(positionLeft)
 
   let div = document.createElement("div");
   div.setAttribute("class", "water");
   document.body.appendChild(div);
-  div.style.left = left;
+  div.style.left = left.toFixed(2) + "%";
   div.style.top = top;
 
   let h2 = document.createElement("h2");
   h2.setAttribute("class", "sentence");
   document.body.appendChild(h2);
   h2.innerHTML = sentence;
-  h2.style.left = left;
+  h2.style.left = left.toFixed(2) + "%";
   h2.style.top = top;
 
   setTimeout(function () {
     document.body.removeChild(div);
     document.body.removeChild(h2);
+    positionLeft.splice(positionLeft.indexOf(left), 1);
   }, animationTime);
 }
 
 function createSentence() {
   getSentence().then((res) => {
-    let sentence = res.book.書き出し + "――" + res.book.姓名;
+    let sentence = res.book.書き出し + "\n" + "――" + res.book.作品名;
+    sentence.replace(/\r?\n/g, "<br>");
     createSentenceElement(sentence);
   });
 }
